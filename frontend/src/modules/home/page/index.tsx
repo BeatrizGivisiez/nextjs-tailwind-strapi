f5ec44f40a34b36a4c74f490ccd06490586b626a
drethesilva <andressilva03@gmail.com>
2025-03-24 11:29:28 +0000

    Project Theme Config

--- frontend/src/modules/home/page/index.tsx
+++ frontend/src/modules/home/page/index.tsx
@@ -3,5 +3,17 @@
 import { TITLE } from "../constants";
 
 export const HomePage = () => {
-  return <div>{TITLE}</div>;
+  return (
+    <div>
+      {" "}
+      <div className="min-h-screen bg-printscreen-50 text-printscreen-900 p-8">
+        <h1 className="text-4xl font-heading">Hello World</h1>
+        <p className="text-lg font-body mt-4">
+          This is using Poppins for body text and Inter for headings.
+        </p>
+        <button className="btn btn-primary mt-6">Click me</button>
+      </div>
+      {TITLE}
+    </div>
+  );
 };
